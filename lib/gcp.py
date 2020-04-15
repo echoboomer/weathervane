e07e5ed6782ec312c71e30cfd8b49c7c11a79e63
Scott Hawkins <scott@echoboomer.net>
2020-04-15 08:46:15 -0400

    Fmt

--- lib/gcp.py
+++ lib/gcp.py
@@ -3,16 +3,19 @@ import requests
 from termcolor import colored
 from .utils import errcolor
 
+
 def get_gcp_status(gcp_api):
-  print(colored('Google Cloud Platform', 'blue', attrs=['bold']) + ' most recent incidents:')
-  print('-------------------------')
+    print(colored('Google Cloud Platform', 'blue',
+                  attrs=['bold']) + ' most recent incidents:')
+    print('-------------------------')
 
-  ir = requests.get('{}/incidents.json'.format(gcp_api))
-  ipr = json.loads(ir.text)
+    ir = requests.get('{}/incidents.json'.format(gcp_api))
+    ipr = json.loads(ir.text)
 
-  for inc in ipr[0:3]:
-    print(colored('Description: ', attrs=['bold']) + inc['external_desc'])
-    print(colored('Created: ', attrs=['bold']) + inc['begin'])
-    print(colored('Last comment: ', attrs=['bold']) + inc['most-recent-update']['text'])
+    for inc in ipr[0:3]:
+        print(colored('Description: ', attrs=['bold']) + inc['external_desc'])
+        print(colored('Created: ', attrs=['bold']) + inc['begin'])
+        print(colored('Last comment: ', attrs=[
+              'bold']) + inc['most-recent-update']['text'])
+        print()
     print()
-  print()
